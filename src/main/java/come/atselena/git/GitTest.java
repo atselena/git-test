a535d7496db50cba6e6861ae2f69584a70ad4f9e
atselena <109396215+atselena@users.noreply.github.com>
2022-07-16 15:45:51 +0800

    pull commit

--- src/main/java/come/atselena/git/GitTest.java
+++ src/main/java/come/atselena/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master commit!");
         System.out.println("hot-fix commit!");
         System.out.println("push commit!");
+        System.out.println("pull commit!");
     }
 
 }
