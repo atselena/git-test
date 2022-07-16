d83bb13d8e75f568ff2a88146d45661c30ed02de
Selena <2947332024@qq.com>
2022-07-16 14:21:22 +0800

    second commit

--- src/main/java/come/atselena/git/GitTest.java
+++ src/main/java/come/atselena/git/GitTest.java
@@ -3,6 +3,7 @@ package come.atselena.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 
 }
